94d8e0349c71022f5357978c20b323e909cb7546
RblRecon <lucas.vdd@outlook.fr>
2018-03-05 14:19:08 +0100

    Add files via upload

--- tinyMVC-bootstrap/templates/accueil.php
+++ tinyMVC-bootstrap/templates/accueil.php
@@ -1,13 +1,4 @@
 <?php
-
-//C'est la propriété php_self qui nous l'indique : 
-// Quand on vient de index : 
-// [PHP_SELF] => /chatISIG/index.php 
-// Quand on vient directement par le répertoire templates
-// [PHP_SELF] => /chatISIG/templates/accueil.php
-
-// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-// Pas de soucis de bufferisation, puisque c'est dans le cas où on appelle directement la page sans son contexte
 if (basename($_SERVER["PHP_SELF"]) != "index.php")
 {
 	header("Location:../index.php?view=accueil");
@@ -18,8 +9,8 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 
     <div class="page-header">
-      <h1>Titre</h1>
+      <h1>Title</h1>
     </div>
 
-    <p class="lead">Contenu </p>
+    <p class="lead">Content </p>
 
--- tinyMVC-bootstrap/templates/login.php
+++ tinyMVC-bootstrap/templates/login.php
@@ -14,19 +14,19 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 
 if(secure("msg")){
-$msg=urldecode(secure("msg"));
-echo '<div class="alert alert-danger">
-    <button type="button" aria-hidden="true" class="close">
-    <i class="now-ui-icons ui-1_simple-remove"></i>
-    </button>
-    <span>
-    <b> Warning - </b> '.$msg.'
+  $msg=urldecode(secure("msg"));
+  echo '<br><div class="alert alert-danger">
+  <button type="button" aria-hidden="true" class="close">
+  <i class="nc-icon nc-simple-remove"></i>
+  </button>
+  <span>
+  <b> Warning - </b> '.$msg.'
   </span>
-</div>';
+  </div>';
 }
 ?>
 <div class="page-header">
-	<h1>Identification</h1>
+	<h1>Login</h1>
 </div>
 
 <p class="lead">
@@ -40,7 +40,7 @@ echo '<div class="alert alert-danger">
     <label for="pwd">Password</label>
     <input type="password" class="form-control" name="password" value=""/>
   </div>
-  <button type="submit" name="action" value="Identification" class="btn btn-default">Login</button>
+  <button type="submit" name="action" value="Identification" class="btn btn-default">Enter</button>
 </form>
 
 </p>
